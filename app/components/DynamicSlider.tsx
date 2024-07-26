3132ebe4d279b66877fd3b490268a6fd21b784f9
deffy316 <phoompanna@gmail.com>
2024-07-26 10:26:05 +0200

    FIx DynamicSlider.tsx

--- app/components/DynamicSlider.tsx
+++ app/components/DynamicSlider.tsx
@@ -38,7 +38,7 @@ const DynamicSlider: NextPage = () => {
         type="button"
         className="size-10 inline-flex justify-center items-center gap-x-2 text-sm font-medium last:rounded-e-lg bg-violet-800 text-white hover:bg-violet-600 disabled:opacity-50 disabled:pointer-events-none"
         onClick={handleDecrement}>
-        <svg className="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <svg className="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="M5 12h14"></path>
         </svg>
         </button>
@@ -46,7 +46,7 @@ const DynamicSlider: NextPage = () => {
         type="button"
         className="size-10 inline-flex justify-center items-center gap-x-2 text-sm font-medium last:rounded-e-lg bg-violet-800 text-white hover:bg-violet-600 disabled:opacity-50 disabled:pointer-events-none"
         onClick={handleIncrement}>
-        <svg className="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <svg className="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="M5 12h14"></path>
           <path d="M12 5v14"></path>
         </svg>
